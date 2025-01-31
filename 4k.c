16058508cd9998344873b2d8dd554f911221921b
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-01-31 19:35:50 +0200

    Retune without specifying tempo
    Bench 9440673

--- 4k.c
+++ 4k.c
@@ -685,23 +685,23 @@ static void generate_piece_moves(Move *const restrict movelist,
   return nodes;
 }
 
-__attribute__((aligned(8))) static const i16 material[] = {99,  292, 318,
-                                                           495, 952, 0};
+__attribute__((aligned(8))) static const i16 material[] = {98,  287, 312,
+                                                           488, 941, 0};
 __attribute__((aligned(8))) static const i8 pst_rank[] = {
-    0,   -11, -13, -12, -2, 38, 116, 0,   // Pawn
-    -32, -17, -1,  13,  25, 28, 8,   -25, // Knight
-    -23, -5,  4,   9,   13, 14, 1,   -12, // Bishop
-    -18, -23, -21, -8,  9,  19, 24,  18,  // Rook
-    -23, -15, -10, -3,  8,  18, 8,   17,  // Queen
-    -18, -12, -6,  5,   17, 23, 12,  -15, // King
+    0,   -10, -13, -12, -2, 37, 113, 0,   // Pawn
+    -31, -16, -1,  13,  24, 27, 8,   -24, // Knight
+    -22, -4,  4,   9,   13, 13, 0,   -12, // Bishop
+    -17, -23, -20, -8,  8,  18, 23,  19,  // Rook
+    -22, -14, -9,  -3,  7,  16, 6,   19,  // Queen
+    -18, -12, -6,  4,   16, 21, 11,  -16, // King
 };
 __attribute__((aligned(8))) static const i8 pst_file[] = {
     -3,  3,  -5, -2, -1, 1,  13, -6,  // Pawn
-    -27, -7, 6,  15, 14, 12, 1,  -14, // Knight
-    -12, 0,  2,  5,  6,  1,  5,  -7,  // Bishop
+    -27, -7, 6,  14, 13, 12, 1,  -13, // Knight
+    -13, -1, 3,  5,  6,  2,  5,  -7,  // Bishop
     -6,  1,  6,  8,  6,  3,  -1, -16, // Rook
-    -21, -9, 2,  5,  4,  6,  6,  6,   // Queen
-    -13, 3,  1,  -1, -2, -3, 7,  -9,  // King
+    -21, -9, 2,  5,  4,  6,  6,  7,   // Queen
+    -13, 2,  0,  -2, -3, -3, 7,  -9,  // King
 };
 
 static i32 eval(Position *const restrict pos) {
