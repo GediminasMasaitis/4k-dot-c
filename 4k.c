3b0871006e2ed97f2fce26387a98fbe96dcd995d
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-02-05 03:50:41 +0200

    Castle ordering
    Bench 14956082

--- 4k.c
+++ 4k.c
@@ -650,15 +650,15 @@ static void generate_piece_moves(Move *const restrict movelist,
                       nw(pawns) & (pos->colour[1] | pos->ep), -7);
   generate_pawn_moves(pos, movelist, &num_moves,
                       ne(pawns) & (pos->colour[1] | pos->ep), -9);
-  generate_piece_moves(movelist, &num_moves, pos, to_mask);
   if (!only_captures && pos->castling[0] && !(all & 0x60ull) &&
       !is_attacked(pos, 4, true) && !is_attacked(pos, 5, true)) {
-    movelist[num_moves++] = (Move){4, 6, None, None};
+      movelist[num_moves++] = (Move){ 4, 6, None, None };
   }
   if (!only_captures && pos->castling[1] && !(all & 0xEull) &&
       !is_attacked(pos, 4, true) && !is_attacked(pos, 3, true)) {
-    movelist[num_moves++] = (Move){4, 2, None, None};
+      movelist[num_moves++] = (Move){ 4, 2, None, None };
   }
+  generate_piece_moves(movelist, &num_moves, pos, to_mask);
 
   assert(num_moves < 256);
   return num_moves;
