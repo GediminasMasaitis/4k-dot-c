a3be1ac7b10186f60563afebf3f5ea6a8bdb8e04
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-02-07 00:45:44 +0200

    Smaller repetition detection
    Bench 14133336

--- 4k.c
+++ 4k.c
@@ -779,7 +779,7 @@ static i32 search(Position *const restrict pos, const i32 ply, i32 depth,
 
   // FULL REPETITION DETECTION
   const bool in_qsearch = depth <= 0;
-  for (i32 i = pos_history_count + ply; !in_qsearch && i > 0 && ply > 0;
+  for (i32 i = pos_history_count + ply; i > 0 && ply > 0 && !in_qsearch;
        i -= 2) {
     if (position_equal(pos, &stack[i].history)) {
       return 0;
