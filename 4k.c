634a75f22acd57a331649d8954d53952b7389fee
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-02-08 14:35:36 +0200

    Bishop pair evaluation
    Bench 14434289

--- 4k.c
+++ 4k.c
@@ -692,28 +692,32 @@ static void generate_piece_moves(Move *const restrict movelist,
   return nodes;
 }
 
-__attribute__((aligned(8))) static const i16 material[] = {0,   99,  292, 318,
-                                                           495, 952, 0};
+__attribute__((aligned(8))) static const i16 material[] = {0,   100, 294, 308,
+                                                           496, 954, 0};
 __attribute__((aligned(8))) static const i8 pst_rank[] = {
-    0,   -11, -13, -12, -2, 38, 116, 0,   // Pawn
-    -32, -17, -1,  13,  25, 28, 8,   -25, // Knight
-    -23, -5,  4,   9,   13, 14, 1,   -12, // Bishop
+    0,   -10, -13, -12, -2, 38, 116, 0,   // Pawn
+    -31, -16, 0,   14,  25, 27, 7,   -26, // Knight
+    -25, -7,  3,   9,   13, 15, 2,   -10, // Bishop
     -18, -23, -21, -8,  9,  19, 24,  18,  // Rook
     -23, -15, -10, -3,  8,  18, 8,   17,  // Queen
     -18, -12, -6,  5,   17, 23, 12,  -15, // King
 };
 __attribute__((aligned(8))) static const i8 pst_file[] = {
-    -3,  3,  -5, -2, -1, 1,  13, -6,  // Pawn
+    -4,  3,  -5, -2, -1, 1,  13, -6,  // Pawn
     -27, -7, 6,  15, 14, 12, 1,  -14, // Knight
-    -12, 0,  2,  5,  6,  1,  5,  -7,  // Bishop
-    -6,  1,  6,  8,  6,  3,  -1, -16, // Rook
-    -21, -9, 2,  5,  4,  6,  6,  6,   // Queen
-    -13, 3,  1,  -1, -2, -3, 7,  -9,  // King
+    -12, 0,  2,  5,  6,  2,  5,  -7,  // Bishop
+    -6,  1,  6,  8,  6,  3,  -1, -17, // Rook
+    -21, -9, 2,  5,  4,  6,  6,  7,   // Queen
+    -13, 3,  1,  -1, -3, -3, 7,  -9,  // King
 };
 
 static i32 eval(const Position *const restrict pos) {
   i32 score = 16;
   for (i32 c = 0; c < 2; c++) {
+    if (count(pos->colour[c] & pos->pieces[Bishop]) == 2) {
+      score += 33;
+    }
+
     const i32 sq_xor = c * 56;
     for (i32 p = Pawn; p <= King; p++) {
       u64 copy = pos->colour[c] & pos->pieces[p];
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2593,3 +2593,45 @@ Games | N: 71168 W: 24953 L: 25396 D: 20819
 Penta | [3981, 7360, 13218, 7171, 3854]
 https://gedas.pythonanywhere.com/test/221/
 ```
+
+### 0.99
+
+Bishop pair evaluation
+
+64 bit: 4088 bytes (+24)
+Fill: 2+6
+
+MD5: 4fe6511f871edba5485a4169310e93dd
+
+```
+info depth 1 score cp 28 time 0 nodes 20 pv b1c3
+info depth 2 score cp 16 time 0 nodes 76 pv b1c3
+info depth 3 score cp 26 time 0 nodes 514 pv b1c3
+info depth 4 score cp 16 time 0 nodes 1442 pv b1c3
+info depth 5 score cp 0 time 1 nodes 5509 nps 5509000 pv b1c3
+info depth 6 score cp 16 time 3 nodes 16815 nps 5605000 pv b1c3
+info depth 7 score cp 7 time 8 nodes 54682 nps 6835250 pv b1c3
+info depth 8 score cp 16 time 27 nodes 183693 nps 6803444 pv b1c3
+info depth 9 score cp 8 time 95 nodes 642517 nps 6763336 pv b1c3
+info depth 10 score cp 9 time 413 nodes 2671327 nps 6468104 pv b1c3
+info depth 11 score cp 12 time 2337 nodes 14434289 nps 6176418 pv b1c3
+bestmove b1c3
+14434289 nodes 6176418 nps
+```
+
+```
+Elo   | 9.14 +- 6.07 (95%)
+SPRT  | 10.0+0.10s Threads=1 Hash=1MB
+LLR   | 2.97 (-2.94, 2.94) [0.00, 5.00]
+Games | N: 8024 W: 2852 L: 2641 D: 2531
+Penta | [390, 826, 1432, 911, 453]
+https://gedas.pythonanywhere.com/test/227/
+```
+
+```
+Score of 4k.c-0.99 vs stash15: 477 - 378 - 145  [0.549] 1000
+...      4k.c-0.99 playing White: 281 - 145 - 74  [0.636] 500
+...      4k.c-0.99 playing Black: 196 - 233 - 71  [0.463] 500
+...      White vs Black: 514 - 341 - 145  [0.587] 1000
+Elo difference: 34.5 +/- 20.0, LOS: 100.0 %, DrawRatio: 14.5 %
+```
