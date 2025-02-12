bb6dd3ac7bd039c18f0fda230e0b9e45c31ed845
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-02-12 14:07:35 +0200

    FFP in QS
    Bench 14020382

--- 4k.c
+++ 4k.c
@@ -849,8 +849,8 @@ static i32 search(Position *const restrict pos, const i32 ply, i32 depth,
     // FORWARD FUTILITY PRUNING
     const i32 gain = material[stack[ply].moves[move_index].takes_piece] +
                      material[stack[ply].moves[move_index].promo];
-    if (depth < 8 && !in_qsearch && !in_check && moves_evaluated &&
-        static_eval + 128 * depth + gain < alpha) {
+    if (depth < 8 && !in_check && moves_evaluated &&
+        static_eval + 128 * (in_qsearch ? 1 : depth) + gain < alpha) {
       break;
     }
 
