a30bdba4fa5f8f4ad1155c55e7c6591d44ad4110
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-03-19 12:05:05 +0200

    LMR from 7
    Bench 27571175

--- 4k.c
+++ 4k.c
@@ -867,7 +867,7 @@ static i32 search(Position *const restrict pos, const i32 ply, i32 depth,
     moves_evaluated++;
 
     // LATE MOVE REDCUCTION
-    i32 reduction = depth > 1 && moves_evaluated > 6
+    i32 reduction = depth > 1 && moves_evaluated > 7
                         ? 1 + (alpha == beta - 1) + moves_evaluated / 16
                         : 1;
 
