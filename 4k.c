ef7ca82374e7a9553d11829e292db6f65aef5d61
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-03-21 01:40:01 +0200

    Bishop pair eval
    Bench 27190844

--- 4k.c
+++ 4k.c
@@ -708,26 +708,27 @@ static void generate_piece_moves(Move *const restrict movelist,
   return nodes;
 }
 
-__attribute__((aligned(8))) static const i16 material[] = {0,   89,  296, 325,
-                                                           485, 961, 0};
+__attribute__((aligned(8))) static const i16 material[] = {0,   89,  299, 314,
+                                                           486, 963, 0};
 __attribute__((aligned(8))) static const i8 pst_rank[] = {
     0,   -10, -12, -11, -3, 36, 113, 0,   // Pawn
-    -32, -17, -1,  14,  25, 27, 8,   -25, // Knight
-    -23, -5,  4,   9,   12, 13, 1,   -13, // Bishop
+    -31, -16, 0,   14,  25, 26, 7,   -26, // Knight
+    -25, -7,  4,   9,   13, 14, 2,   -10, // Bishop
     -15, -22, -20, -9,  9,  17, 22,  17,  // Rook
-    -21, -14, -9,  -2,  8,  16, 6,   17,  // Queen
-    -20, -12, -5,  6,   17, 23, 13,  -14, // King
+    -22, -14, -9,  -2,  8,  16, 6,   17,  // Queen
+    -20, -12, -5,  6,   17, 23, 14,  -13, // King
 };
 __attribute__((aligned(8))) static const i8 pst_file[] = {
-    -2,  3,  -4, -2, -1, 5,  10, -8,  // Pawn
-    -28, -7, 6,  15, 14, 12, 1,  -14, // Knight
-    -13, 0,  3,  6,  6,  1,  5,  -7,  // Bishop
+    -2,  3,  -5, -2, -1, 5,  10, -9,  // Pawn
+    -28, -7, 6,  16, 15, 12, 1,  -14, // Knight
+    -13, 0,  3,  6,  6,  1,  5,  -8,  // Bishop
     -4,  0,  3,  4,  3,  6,  0,  -13, // Rook
-    -21, -9, 1,  4,  3,  7,  7,  8,   // Queen
-    -13, 2,  1,  1,  -1, -3, 6,  -10, // King
+    -21, -9, 1,  4,  4,  7,  7,  8,   // Queen
+    -13, 2,  1,  0,  -1, -3, 6,  -10, // King
 };
-__attribute__((aligned(8))) static const i8 open_files[] = {0,  15, -1, -3,
+__attribute__((aligned(8))) static const i8 open_files[] = {0,  15, -2, -2,
                                                             22, 8,  -7};
+static const i8 bishop_pair = 36;
 
 static i32 eval(Position *const restrict pos) {
   i32 score = 16;
@@ -755,6 +756,11 @@ static i32 eval(Position *const restrict pos) {
         score += pst_rank[(p - 1) * 8 + rank];
         score += pst_file[(p - 1) * 8 + file];
       }
+    }
+
+    // BISHOP PAIR
+    if (count(pos->colour[0] & pos->pieces[Bishop]) > 1) {
+      score += bishop_pair;
     }
 
     score = -score;
