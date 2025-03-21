2973f022a427c363a40d9dd03fdf88c2908ccd0c
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-03-21 02:08:28 +0200

    Protected pawns eval
    Bench 22525797

--- 4k.c
+++ 4k.c
@@ -708,32 +708,37 @@ static void generate_piece_moves(Move *const restrict movelist,
   return nodes;
 }
 
-__attribute__((aligned(8))) static const i16 material[] = {0,   89,  296, 325,
-                                                           485, 961, 0};
+__attribute__((aligned(8))) static const i16 material[] = {0,   88,  296, 326,
+                                                           487, 965, 0};
 __attribute__((aligned(8))) static const i8 pst_rank[] = {
-    0,   -10, -12, -11, -3, 36, 113, 0,   // Pawn
-    -32, -17, -1,  14,  25, 27, 8,   -25, // Knight
-    -23, -5,  4,   9,   12, 13, 1,   -13, // Bishop
-    -15, -22, -20, -9,  9,  17, 22,  17,  // Rook
-    -21, -14, -9,  -2,  8,  16, 6,   17,  // Queen
-    -20, -12, -5,  6,   17, 23, 13,  -14, // King
+    0,   -9,  -15, -12, -2, 38, 114, 0,   // Pawn
+    -32, -17, -1,  14,  26, 28, 8,   -25, // Knight
+    -23, -5,  4,   9,   13, 14, 1,   -13, // Bishop
+    -15, -23, -20, -8,  9,  17, 22,  17,  // Rook
+    -22, -14, -9,  -2,  8,  16, 6,   17,  // Queen
+    -20, -12, -5,  6,   18, 23, 13,  -14, // King
 };
 __attribute__((aligned(8))) static const i8 pst_file[] = {
-    -2,  3,  -4, -2, -1, 5,  10, -8,  // Pawn
-    -28, -7, 6,  15, 14, 12, 1,  -14, // Knight
-    -13, 0,  3,  6,  6,  1,  5,  -7,  // Bishop
-    -4,  0,  3,  4,  3,  6,  0,  -13, // Rook
-    -21, -9, 1,  4,  3,  7,  7,  8,   // Queen
-    -13, 2,  1,  1,  -1, -3, 6,  -10, // King
+    -1,  1,  -4, -2, -1, 5,  8, -7,  // Pawn
+    -29, -8, 6,  15, 15, 13, 2, -14, // Knight
+    -14, 0,  3,  6,  7,  1,  5, -8,  // Bishop
+    -4,  0,  3,  4,  3,  6,  0, -13, // Rook
+    -22, -9, 1,  4,  4,  7,  7, 8,   // Queen
+    -14, 2,  1,  0,  -1, -2, 6, -10, // King
 };
-__attribute__((aligned(8))) static const i8 open_files[] = {0,  15, -1, -3,
+__attribute__((aligned(8))) static const i8 open_files[] = {0,  12, -1, -3,
                                                             22, 8,  -7};
+static const i8 protected_pawn = 10;
 
 static i32 eval(Position *const restrict pos) {
   i32 score = 16;
   for (i32 c = 0; c < 2; c++) {
 
     const u64 own_pawns = (pos->colour[0] & pos->pieces[Pawn]);
+
+    // PROTECTED PAWNS
+    score +=
+        protected_pawn * count(own_pawns & (nw(own_pawns) | ne(own_pawns)));
 
     for (i32 p = Pawn; p <= King; p++) {
       u64 copy = pos->colour[0] & pos->pieces[p];
