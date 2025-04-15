cdb1c875d845e67086dc575daf83da16d57b784d
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-04-16 01:50:30 +0300

    LMR alt
    Bench 16344571

--- 4k.c
+++ 4k.c
@@ -979,7 +979,7 @@ static i16 search(Position *const restrict pos, const i32 ply, i32 depth,
 
     // LATE MOVE REDCUCTION
     i32 reduction =
-        depth > 1 && moves_evaluated > 6 ? 2 + moves_evaluated / 16 : 1;
+        depth > 1 && moves_evaluated >= 8 ? 1 + moves_evaluated / 8 : 1;
 
     i32 score;
     while (true) {
