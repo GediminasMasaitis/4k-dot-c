bc1fe1ad567b23a3c4df80807328ec32421df9cd
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-04-23 23:00:22 +0300

    Compression stub

--- loader.c
+++ loader.c
@@ -1,13 +1,16 @@
-__attribute__((section(".payload"), used))
-//__attribute__((aligned(8)))
-char payload[] = {
+const char compressed[] = {
 #embed "./build/4kc"
 };
+
+__attribute__((section(".payload"), used))
+char decompressed[4096];
 
 void _start() {
   //__asm__ volatile (
   //  "jmp 4200827"
   //);
+
+  __builtin_memcpy(decompressed, compressed, sizeof(compressed));
 
   ((void (*)(void))(0x400d5d))();
 }
