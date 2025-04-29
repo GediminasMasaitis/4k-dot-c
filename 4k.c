dd57464c4ec684f4fed1513379ac383ee1d6eb13
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-04-29 13:16:37 +0300

    LMP moves 1
    Bench 19021348

--- 4k.c
+++ 4k.c
@@ -1045,7 +1045,7 @@ static i16 search(Position *const restrict pos, const i32 ply, i32 depth,
 
     // LATE MOVE PRUNING
     if (!in_check && alpha == beta - 1 &&
-        quiets_evaluated > 3 + depth * depth) {
+        quiets_evaluated > 1 + depth * depth) {
       break;
     }
   }
