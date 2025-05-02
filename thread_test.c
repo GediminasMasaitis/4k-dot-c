5972a2232d210a7b6720775f61f8581d341fafab
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-05-02 10:54:40 +0300

    cleaner

--- thread_test.c
+++ thread_test.c
@@ -2,18 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sched.h>
-#include <sys/mman.h>
 #include <sys/syscall.h>
 
 enum { thread_count = 4 };
 enum { thread_stack_size = 8 * 1024 * 1024 };
 
-typedef struct {
-  char bytes[thread_stack_size];
-} thread_stack __attribute__((aligned(4096)));
-
-thread_stack thread_stacks[thread_count];
+__attribute__((aligned(4096))) char thread_stacks[thread_count][thread_stack_size];
 
 struct __attribute((aligned(16))) stack_head {
   void (*entry)(struct stack_head*);
@@ -21,8 +15,7 @@ struct __attribute((aligned(16))) stack_head {
   // ...
 };
 
-__attribute((naked))
-static long newthread(struct stack_head* stack)
+__attribute((naked)) static long newthread(struct stack_head* stack)
 {
   __asm volatile (
   "mov  %%rdi, %%rsi\n"     // arg2 = stack
@@ -37,11 +30,9 @@ static long newthread(struct stack_head* stack)
 
 static void threadentry(struct stack_head* stack)
 {
-  //while (true)
   for (int i = 0; i < 5; i++)
   {
     printf("Hello %d from thread %d\n", i, stack->thread_id);
-    //fflush(stdout);
     sleep(1);
   }
 
@@ -53,8 +44,7 @@ int main() {
 
   for (int i = 0; i < thread_count; i++)
   {
-    struct stack_head* stack = (struct stack_head*)(((char*)&thread_stacks[i+1]) - 4096);
-    //struct stack_head* stack = newstack(stack_size);
+    struct stack_head* stack = (struct stack_head*)&thread_stacks[i + 1][-4096];
     stack->entry = threadentry;
     stack->thread_id = i;
     newthread(stack);
@@ -65,9 +55,7 @@ int main() {
   {
     getc(stdin);
     printf("Hello from main\n");
-    //fflush(stdout);
   }
 
-  //getchar();  // Wait for input
   return 0;
 }
