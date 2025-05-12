f1674abaf1724ddb7f2619b2ce211a82e02e52ec
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-05-12 14:18:13 +0300

    LVA
    Bench 25319794

--- 4k.c
+++ 4k.c
@@ -1078,8 +1078,9 @@ static i16 search(Position *const restrict pos, const i32 ply, i32 depth,
       const i32 order_move_score =
           ((i32)move_equal(&tt_move, &stack[ply].moves[order_index])
            << 30) // PREVIOUS BEST MOVE FIRST
-          + (i32)stack[ply].moves[order_index].takes_piece * 921 +
-          (i32)move_equal(&stack[ply].killer, &stack[ply].moves[order_index]) *
+          + (i32)stack[ply].moves[order_index].takes_piece * 921 // MVV
+          - (i32)piece_on(pos, stack[ply].moves[order_index].from) * 64 // MVV
+          + (i32)move_equal(&stack[ply].killer, &stack[ply].moves[order_index]) *
               915 // KILLER MOVE
           +
           move_history[pos->flipped][stack[ply].moves[order_index].takes_piece]
