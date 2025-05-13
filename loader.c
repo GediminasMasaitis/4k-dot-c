83cbbf24210a78249d44b1286e180b9a9b8b75cc
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-05-13 14:41:20 +0300

    Disable >32kb case in aPLib
    Bench 15170468

--- loader.c
+++ loader.c
@@ -5,11 +5,11 @@ const unsigned char payload_compressed[] = {
 __attribute__((section(".payload"),
                used)) unsigned char payload_decompressed[4096 * 2];
 
-//#include "aplib.h"
-void aP_depack_asm(const void* source, void* destination);
+// #include "aplib.h"
+void aP_depack_asm(const void *source, void *destination);
 
 void _start() {
-  //decompress_aplib(payload_decompressed, payload_compressed);
+  // decompress_aplib(payload_decompressed, payload_compressed);
   aP_depack_asm(payload_compressed, payload_decompressed);
 
   __asm__ volatile("jmp " PAYLOAD_START);
