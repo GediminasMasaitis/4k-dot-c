1eaf16e167c46b550a06c106d642d4dfd6616102
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-05-23 13:04:04 +0300

    NMP extra check
    Bench 9115117

--- 4k.c
+++ 4k.c
@@ -1052,7 +1052,7 @@ static i16 search(Position *const restrict pos, const i32 ply, i32 depth,
     }
 
     // NULL MOVE PRUNING
-    if (do_null && depth > 2 && static_eval >= beta) {
+    if (do_null && depth > 2 && static_eval >= beta && static_eval >= stack[ply].static_eval) {
       Position npos = *pos;
       flip_pos(&npos);
       npos.ep = 0;
