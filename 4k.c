9498800f2bcd629bfb2e9ba5ba5d7280a11160d2
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-05-29 10:12:22 +0300

    Razoring 192
    Bench 19592875

--- 4k.c
+++ 4k.c
@@ -1125,7 +1125,7 @@ static i16 search(Position *const restrict pos, const i32 ply, i32 depth,
       }
 
       // RAZORING
-      in_qsearch = static_eval + 131 * depth <= alpha;
+      in_qsearch = static_eval + 192 * depth <= alpha;
     }
 
     // NULL MOVE PRUNING
@@ -1179,11 +1179,12 @@ static i16 search(Position *const restrict pos, const i32 ply, i32 depth,
     }
 
     // FORWARD FUTILITY PRUNING / DELTA PRUNING
-    if (depth < 8 && !in_check && moves_evaluated &&
-        static_eval + 128 * depth +
+    if (!in_check &&
+        static_eval + (in_qsearch ? 32 : 128 * depth) +
                 max_material[stack[ply].moves[move_index].takes_piece] +
                 max_material[stack[ply].moves[move_index].promo] <
-            alpha) {
+            alpha &&
+        moves_evaluated && depth < 8) {
       break;
     }
 
