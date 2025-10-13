a98f47d98d799fd89f9a55c7df2f58f0053fab8c
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-10-13 17:19:57 +0300

    Update aplib.asm

--- aplib.asm
+++ aplib.asm
@@ -19,106 +19,108 @@ public decompress_aplib
 
 section '.text'
 decompress_aplib:
-    ; push   rbx ; Uncomment to preserve System V calling convention
+    mov     al,080H         ; clear bit queue(al) and set high bit to move into carry
+    xor     edx, edx        ; invalidate rep offset in edx
 
-    ; cld ; Uncomment for robustness, though in my loader it's not needed
-    mov    dl, 0x80
-    xor    ebx, ebx
-    lea    r11, [getbit]
-
-literal:
-    movsb
-    mov    bl, 2
-nexttag:
-    call   r11 ; getbit
-    jnc    literal
-
-    xor    ecx, ecx
-    call   r11 ; getbit
-    jnc    codepair
-    xor    eax, eax
-    call   r11 ; getbit
-    jnc    shortmatch
-    mov    bl, 2
-    inc    ecx
-    mov    al, 10h
-  .getmorebits:
-    call   r11 ; getbit
-    adc    al, al
-    jnc    .getmorebits
-    jnz    domatch
-    stosb
-    jmp    nexttag
-codepair:
-    call   getgamma_no_ecx
-    sub    ecx, ebx
-    jnz    normalcodepair
-    call   getgamma
-    jmp    domatch_lastpos
-
-shortmatch:
-    lodsb
-    shr    eax, 1
-    jz     donedepacking
-    adc    ecx, ecx
-    jmp    domatch_with_2inc
-
-normalcodepair:
-    xchg   eax, ecx
-    dec    eax
-    shl    eax, 8
-    lodsb
-    call   getgamma
-
-    ; Uncomment the following 2 instructions if >32kb
-    ; cmp    eax, 32000
-    ; jae    short domatch_with_2inc
-    cmp    ah, 5
-    jae    short domatch_with_inc
-    cmp    eax, 7fh
-    ja     short domatch_new_lastpos
-
-domatch_with_2inc:
-    inc    ecx
-
-domatch_with_inc:
-    inc    ecx
-
-domatch_new_lastpos:
-    xchg   eax, ebp ; Was: xchg eax, r8d
-domatch_lastpos:
-    mov    eax, ebp ; Was: mov eax, r8d
-
-    mov    bl, 1
-
-domatch:
-    push   rsi
-    mov    rsi, rdi
-    sub    rsi, rax
-    rep    movsb
-    pop    rsi
-    jmp    nexttag
-
-getbit:
-    add    dl, dl
-    jnz    .stillbitsleft
-    mov    dl, [rsi]
-    inc    rsi
-    adc    dl, dl
-  .stillbitsleft:
-    ret
-
-getgamma:
-    xor    ecx, ecx
-getgamma_no_ecx:
-    inc    ecx
-  .getgammaloop:
-    call   r11 ; getbit
-    adc    ecx, ecx
-    call   r11 ; getbit
-    jc     .getgammaloop
-    ret
-
-donedepacking:
-    ; pop    rbx ; Uncomment to preserve System V calling convention
-    ret
+    call    .init_get_bit
+.get_dibits:
+        call    rbp             ; read data bit
+        adc     ecx,ecx         ; shift into cx
+.get_bit:
+        add     al,al           ; shift bit queue, and high bit into carry
+        jnz     .got_bit        ; queue not empty, bits remain
+        lodsb                   ; read 8 new bits
+        adc     al,al           ; shift bit queue, and high bit into carry
+.got_bit:
+        ret
+.init_get_bit:
+        pop     rbp             ; load offset of .get_bit, to be used with call ebp
+        add     rbp, .get_bit - .get_dibits
+.literal:
+        movsb                   ; read and write literal byte
+.next_command_after_literal:
+        push    03H
+        pop     rbx             ; set follows_literal(bx) to 3
+        
+.next_command:
+        call    rbp             ; read 'literal or match' bit
+        jnc     .literal        ; if 0: literal
+                                
+                                ; 1x: match
+        call    rbp             ; read '8+n bits or other type' bit
+        jc      .other          ; 11x: other type of match
+                                ; 10: 8+n bits match
+        call    .get_gamma2     ; read gamma2-coded high offset bits
+        sub     ecx,ebx         ; high offset bits == 2 when follows_literal == 3 ?
+                                ; (a gamma2 value is always >= 2, so substracting follows_literal when it
+                                ; is == 2 will never result in a negative value)
+        jae     .not_repmatch   ; if not, not a rep-match
+        call    .get_gamma2     ; read match length
+        jmp     .got_len        ; go copy
+.not_repmatch:
+        mov     edx,ecx         ; transfer high offset bits to dh
+        shl     edx,8
+        mov     dl,[esi]        ; read low offset byte in dl
+        inc     esi
+        call    .get_gamma2     ; read match length
+        ;cmp     edx,7D00H       ; offset >= 32000 ?
+        ;jae     .increase_len_by2 ; if so, increase match len by 2
+        cmp     edx,0500H       ; offset >= 1280 ?
+        jae     .increase_len_by1 ; if so, increase match len by 1
+        cmp     edx,0080H       ; offset < 128 ?
+        jae     .got_len        ; if so, increase match len by 2, otherwise it would be a 7+1 copy
+.increase_len_by2:
+        inc     ecx             ; increase length
+.increase_len_by1:
+        inc     ecx             ; increase length
+        ; copy ecx bytes from match offset edx
+.got_len:
+        push    rsi             ; save esi (current pointer to compressed data)
+        mov     esi,edi         ; point to destination in edi - offset in edx
+        sub     esi,edx
+        rep     movsb           ; copy matched bytes
+        pop     rsi             ; restore esi
+        mov     bl,02H          ; set follows_literal to 2 (ebx is unmodified by match commands)
+        jmp     .next_command
+        ; read gamma2-coded value into ecx
+.get_gamma2:
+        xor     ecx,ecx         ; initialize to 1 so that value will start at 2
+        inc     ecx             ; when shifted left in the adc below
+.gamma2_loop:
+        call    .get_dibits     ; read data bit, shift into cx, read continuation bit
+        jc      .gamma2_loop    ; loop until a zero continuation bit is read
+        ret
+        ; handle 7 bits offset + 1 bit len or 4 bits offset / 1 byte copy
+.other:
+        xor     ecx,ecx
+        call    rbp             ; read '7+1 match or short literal' bit
+        jc      .short_literal  ; 111: 4 bit offset for 1-byte copy
+                                ; 110: 7 bits offset + 1 bit length
+                                
+        movzx   edx,byte[esi]   ; read offset + length in dl
+        inc     esi
+        inc     ecx             ; prepare cx for length below
+        shr     dl,1            ; shift len bit into carry, and offset in place
+        je      .done           ; if zero offset: EOD
+        adc     ecx,ecx         ; len in cx: 1*2 + carry bit = 2 or 3
+        jmp     .got_len
+        ; 4 bits offset / 1 byte copy
+.short_literal:
+        call    .get_dibits     ; read 2 offset bits
+        adc     ecx,ecx
+        call    .get_dibits     ; read 2 offset bits
+        adc     ecx,ecx
+        xchg    eax,ecx         ; preserve bit queue in cx, put offset in ax
+        jz      .write_zero     ; if offset is 0, write a zero byte
+                                ; short offset 1-15
+        mov     ebx,edi         ; point to destination in es:di - offset in ax
+        sub     ebx,eax         ; we trash bx, it will be reset to 3 when we loop
+        mov     al,[ebx]        ; read byte from short offset
+.write_zero:
+        stosb                   ; copy matched byte
+        xchg    eax,ecx         ; restore bit queue in al
+        jmp     .next_command_after_literal
+.done:
+        ;sub     edi, [esp+32+8] ; compute decompressed size
+        ;mov     [esp+28], edi
+        ret
