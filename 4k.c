c883c854710963e4badfd54f3ec19df42d0bb85b
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2025-10-17 18:29:23 +0300

    AW base
    Bench 13281744

--- 4k.c
+++ 4k.c
@@ -1279,7 +1279,7 @@ i16 search(H(98, 1, Position *const restrict pos), H(98, 1, i32 alpha),
               G(115, &stack[ply].moves[best_index]));
 
     // FORWARD FUTILITY PRUNING / DELTA PRUNING
-    if (G(116, depth < 8) &&
+    if (G(116, ply > 0) && G(116, depth < 8) &&
         G(116,
           G(117, static_eval + 136 * depth) +
                   G(117, eg.material[stack[ply].moves[move_index].promo]) +
@@ -1442,21 +1442,32 @@ void iteratively_deepen(
     H(126, 1, Position *const restrict pos),
     H(126, 1, SearchStack *restrict stack),
     H(126, 1, const i32 pos_history_count)) {
-  start_time = get_time();
+  G(995, start_time = get_time();)
+  G(995, i32 score = 0;)
 #ifdef FULL
   for (i32 depth = 1; depth < maxdepth; depth++) {
 #else
   for (i32 depth = 1; depth < max_ply; depth++) {
 #endif
-    i32 score =
-        search(H(98, 4, pos), H(98, 4, -inf), H(98, 4, 0), H(98, 4, depth),
-               H(99, 4, stack),
+    G(996, i32 window = 24;)
+    G(996, size_t elapsed;)
+    while (true) {
+      G(997, const i32 alpha = score - window;)
+      G(997, const i32 beta = score + window;)
+      score =
+          search(H(98, 4, pos), H(98, 4, alpha), H(98, 4, 0), H(98, 4, depth),
+                 H(99, 4, stack),
 #ifdef FULL
-               nodes,
+                 nodes,
 #endif
-               H(99, 4, inf), H(99, 4, pos_history_count), H(99, 4, false));
+                 H(99, 4, beta), H(99, 4, pos_history_count), H(99, 4, false));
+      elapsed = get_time() - start_time;
+      G(
+          998, if (G(999, (score > alpha && score < beta)) ||
+                   G(999, elapsed > max_time)) { break; })
+      G(998, window *= 2;)
+    }
 
-    size_t elapsed = get_time() - start_time;
 #ifdef FULL
     // Don't print unreliable scores
     if (elapsed > max_time) {
