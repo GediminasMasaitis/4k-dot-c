1140d6b7beae84ad8fbb7b67a8be3c703113eca9
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2026-03-05 13:52:44 +0200

    Update corrhist on exact
    Bench 13244752

--- 4k.c
+++ 4k.c
@@ -1508,7 +1508,7 @@ i32 search(
                         .flag = tt_flag};
 
   // UPDATE CORRECTION HISTORY
-  if (G(234, (G(235, (G(236, best_score < stack[ply].static_eval) &&
+  if (G(234, (G(235, tt_flag == Exact) || G(235, (G(236, best_score < stack[ply].static_eval) &&
                       G(236, tt_flag == Upper))) ||
               G(235, (G(237, best_score > stack[ply].static_eval) &&
                       G(237, tt_flag == Lower))))) &&
