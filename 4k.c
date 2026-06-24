522c109dbe4a9a6c0c1c5a28231d08c878e600df
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2026-06-25 02:26:00 +0300

    Add version and widen UCI option ranges

--- 4k.c
+++ 4k.c
@@ -1332,11 +1332,11 @@ static void run() {
 #ifdef FULL
     u64 nodes = 0;
     if (!strcmp(line, "uci")) {
-      putl("id name 4k.c\n");
+      putl("id name 4k.c 3.0\n");
       putl("id author Gediminas Masaitis\n");
       putl("\n");
-      putl("option name Hash type spin default 1 min 1 max 1\n");
-      putl("option name Threads type spin default 1 min 1 max 1\n");
+      putl("option name Hash type spin default 64 min 1 max 65536\n");
+      putl("option name Threads type spin default 1 min 1 max 65536\n");
       putl("uciok\n");
     } else if (!strcmp(line, "ucinewgame")) {
       __builtin_memset(tt, 0, tt_length * sizeof(TTEntry));
